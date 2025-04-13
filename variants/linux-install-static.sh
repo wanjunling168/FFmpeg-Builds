3be14e6b66cdd29653a0a86caab0b7e10efcb552
wanjunling168 <wanjunling168@qq.com>
2025-04-13 16:22:54 +0800

    Update linux-install-static.sh

--- variants/linux-install-static.sh
+++ variants/linux-install-static.sh
@@ -7,6 +7,20 @@ package_variant() {
     mkdir -p "$OUT"/bin
     cp "$IN"/bin/* "$OUT"/bin
 
+    mkdir -p "$OUT"/lib
+    cp -a "$IN"/lib/*.so* "$OUT"/lib
+
+    mkdir -p "$OUT"/lib/pkgconfig
+    cp -a "$IN"/lib/pkgconfig/*.pc "$OUT"/lib/pkgconfig
+    sed -i \
+        -e 's|^prefix=.*|prefix=${pcfiledir}/../..|' \
+        -e 's|/ffbuild/prefix|${prefix}|' \
+        -e '/Libs.private:/d' \
+        "$OUT"/lib/pkgconfig/*.pc
+
+    mkdir -p "$OUT"/include
+    cp -r "$IN"/include/* "$OUT"/include
+
     mkdir -p "$OUT/doc"
     cp -r "$IN"/share/doc/ffmpeg/* "$OUT"/doc
 
